6abc759c827907fa390f4707f87a7887e89712c9
Gustavo Prado <57048433+gustavopradobr@users.noreply.github.com>
2023-10-30 13:04:33 -0300

    Corrige foto de perfil
    -Corrige exibição da foto de perfil nas páginas que a contém
    -Adiciona tag de idioma nas páginas

--- scripts/profile.js
+++ scripts/profile.js
@@ -3,7 +3,7 @@ import { getUserData } from "../auth/userData.js";
 (async () => {
   const user = await getUserData();
 
-  const image = document.getElementById("empty-picture");
+  const image = document.getElementById("profile-image");
   image.style = `background-image: url(${user.picture})`;
 
   const inputName = document.querySelector(".username");
