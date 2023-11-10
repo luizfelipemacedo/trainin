f7d4ae4e338d424148e4205fa622234374c2115c
luizfelipemacedoc <114022960+luizfelipemacedoc@users.noreply.github.com>
2023-11-09 21:53:31 -0300

    feat(profile): exclusão e logout do perfil

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,4 +1,39 @@
+import api from "../config/api.js";
+import baseUrl from "../config/baseUrl.js";
 import { getUserData } from "../auth/userData.js";
+
+const deleteButton = document.querySelector("#delete");
+const logoutButton = document.querySelector("#logout");
+
+deleteButton.addEventListener("click", handleDeleteUser);
+logoutButton.addEventListener("click", handleLogout);
+
+async function handleDeleteUser() {
+  const { id } = await getUserData();
+
+  const confirmDelete = confirm("Tem certeza que deseja deletar sua conta?");
+  if (!confirmDelete) return;
+
+  try {
+    const res = await api.delete(`/user/${id}`);
+
+    if (res.status === 204) {
+      alert("Conta deletada com sucesso!");
+
+      handleLogout();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+async function handleLogout() {
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("sb-nutpqerdnsozmmxqzbyj-auth-token");
+
+  const homePage = new URL(`${baseUrl}/pages/login.html`);
+  window.location.assign(homePage.toString());
+};
 
 (async () => {
   const user = await getUserData();
