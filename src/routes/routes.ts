abe20ff250883511a30ee3d349535680bc0a033f
luizfelipemacedo <114022960+luizfelipemacedo@users.noreply.github.com>
2023-11-12 20:22:30 -0300

    feat: rota para criar rotina de treinos

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { deleteUserById } from "../controllers/user";
+import { createRoutine } from "../controllers/workout";
 
 const routes = Router();
 
+//user routes
 routes.delete('/user/:id', deleteUserById);
+
+//workout routes
+routes.post('/workout/create-routine', createRoutine);
 
 export default routes;
